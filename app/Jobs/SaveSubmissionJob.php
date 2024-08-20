fbf40fd8a25f9d5836661f035aa1fdcd13178378
sujeeshhw <sujeesh@happyweddings.com>
2024-08-20 09:47:36 +0530

    adding files

---
+++ app/Jobs/SaveSubmissionJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Submission;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SaveSubmissionJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $data;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        // Save data to the database
+        $submission = Submission::create($this->data);
+
+        // Fire an event
+        event(new \App\Events\SubmissionSaved($submission));
+    }
+}
